59b4e4f8bc0d57e99914c9e1ae7daa39aee207d5
xaskoiking <33464972+xaskoiking@users.noreply.github.com>
2018-09-16 19:27:42 -0400

    Updated Code

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,11 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+d3.select("tbody")
+  .selectAll("tr")
+  .data(tableData)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+  });
