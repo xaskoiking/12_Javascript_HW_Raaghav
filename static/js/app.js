0cbe2845d3c452505ab073ae7a1bd8463dd26824
xaskoiking <33464972+xaskoiking@users.noreply.github.com>
2018-09-16 21:42:12 -0400

    Updated Filter

--- static/js/app.js
+++ static/js/app.js
@@ -10,3 +10,21 @@ d3.select("tbody")
   .html(function(d) {
     return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
   });
+
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("datetime");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
