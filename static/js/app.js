49e8244427eedd6d2ae267dcff87f23b27710239
xaskoiking <33464972+xaskoiking@users.noreply.github.com>
2018-09-18 18:06:03 -0400

    Updated Code

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Loading the Table
 d3.select("tbody")
   .selectAll("tr")
   .data(tableData)
@@ -11,7 +11,8 @@ d3.select("tbody")
     return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
   });
 
-function myFunction() {
+//Logic to search the data based on the date
+function dateSearch() {
   var input, filter, table, tr, td, i;
   input = document.getElementById("datetime");
   filter = input.value.toUpperCase();
@@ -19,6 +20,102 @@ function myFunction() {
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+//Logic to search the data based on the CITY
+function citySearch() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("city");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+//Logic to search the data based on the STATE
+function stateSearch() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("state");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+//Logic to search the data based on the COUNTRY
+function countrySearch() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("country");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+
+//Logic to search the data based on the SHAPE
+function shapeSearch() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("shape");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+
+//Logic to search the data based on the DURATION
+function durationSearch() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("duration");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[5];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
